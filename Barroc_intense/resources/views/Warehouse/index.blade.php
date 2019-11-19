e8094852bd9b8d858f753daac47d875c947ebb5d
Unknown <Jaibreyon.lourens@gmail.com>
2019-11-19 12:52:12 +0100

    Added Migrations

--- Barroc_intense/resources/views/Warehouse/index.blade.php
+++ Barroc_intense/resources/views/Warehouse/index.blade.php
@@ -1,4 +1,14 @@
 @extends('app')
 @section('content')
+<main>
+    <h2>Magazijn</h2>
+    <div class="products">
+        <form action="{{route('WarehouseController')}}" method="post" enctype="multipart/form-data">
+            <div class="product">
+                <p>product</p> <input type="text" name="amount">
+            </div>
+        </form>
 
+    </div>    
+</main>
 @endsection
