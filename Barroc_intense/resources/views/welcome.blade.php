d32c412d85ecdda2e811c2d936a4f638480a17d5
Unknown <h.fm@live.com>
2019-11-22 13:09:06 +0100

    home login en welcome pagina

--- Barroc_intense/resources/views/welcome.blade.php
+++ Barroc_intense/resources/views/welcome.blade.php
@@ -1,29 +1,63 @@
 @extends('app')
 
-@section('content')
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Login</a>
+<nav id="nav" class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img class="Logo-app" src="images/mainlogo.png">
+        {{ config('') }}
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
+
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
                 @endif
-            @endauth
-        </div>
-    @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
 
-    <div class="content">
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
+    </div>
+    </div>
+</nav>
+@section('content')
+<div class="container">
         <div class="title m-b-md">
-            <img class="welcome-img" src="imgs/logoheer.png" alt="Logo">
+            <img class="welcome-img" src="images/logoheer.png" alt="Logo">
         </div>
 
         <div class="welcome-text">
             <h1>Welkom bij de beste koffie leverancier van de benelux!</h1>
         </div>
-    </div>
 </div>
 @endsection
