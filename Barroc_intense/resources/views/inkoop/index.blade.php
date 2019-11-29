732980862d57adcac399aceb4e61aaeaa3ae65c8
Jaibreyon Lourens <jaibreyonlourens@gmail.com>
2019-11-29 13:43:12 +0100

    changed inkoop pagina

--- Barroc_intense/resources/views/inkoop/index.blade.php
+++ Barroc_intense/resources/views/inkoop/index.blade.php
@@ -3,57 +3,38 @@
     <div class="home-page">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-8">
+                <div class="col-md-15">
                     <div id="home-card" class="card">
                         <div class="card-body">
-
-                            <h1>Inkoop</h1>
-
-                            <div class="search-container">
-                                <form action="">
-                                    <input class="input-group-text" type="text" id="search"
-                                           placeholder="Type to search users" autocomplete="off">
-                                </form>
-                                <button class="btn  btn-info"><i class="far fa-search"></i></button>
-                            </div>
-
-
-                            <div class="bestel">
-                                <h2>orders</h2>
-                                <div class="prodcut">
-                                    <button class="btn  btn-info">x</button>
-                                    <h4>prodcut</h4>
-                                    <input class="input-group-text" type="text">
-                                    <button class="btn  btn-info">+</button>
-                                </div>
-                                <div class="prodcut">
-                                    <button class="btn  btn-info">x</button>
-                                    <h4>prodcut</h4>
-                                    <input class="input-group-text" type="text">
-                                    <button class="btn  btn-info">+</button>
-                                </div>
-                                <div class="prodcut">
-                                    <button class="btn  btn-info">x</button>
-                                    <h4>prodcut</h4>
-                                    <input class="input-group-text" type="text">
-                                    <button class="btn  btn-info">+</button>
-                                </div>
-                                <div class="prodcut">
-                                    <button class="btn  btn-info">x</button>
-                                    <h4>prodcut</h4>
-                                    <input class="input-group-text" type="text">
-                                    <button class="btn  btn-info">+</button>
-                                </div>
-                                <div class="prodcut">
-                                    <button class="btn  btn-info">x</button>
-                                    <h4>prodcut</h4>
-                                    <input class="input-group-text" type="text">
-                                    <button class="btn  btn-info">+</button>
-                                </div>
-                            </div>
-                            <button class="btn  btn-info">order</button>
-
-
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Naam</th>
+                                    <th scope="col">Prijs</th>
+                                    <th scope="col">Voorraad</th>
+                                    <th scope="col">Beschikbaar</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($products as $product)
+                                    <tr>
+                                        <th scope="row">{{$product->id}}</th>
+                                        <td><a href="{{route('inkoop.edit', $product->id)}}">{{$product->name}}</a></td>
+                                        <td>{{$product->price}}</td>
+                                        <td>{{$product->units}}
+                                        </td>
+                                        <td>
+                                            @if($product->available === 1)
+                                                
+                                            @else
+                                                niet beschikbaar
+                                            @endif
+                                        </td>
+                                    </tr>
+                            @endforeach
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
