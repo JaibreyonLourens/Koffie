7cc820bc31ba411523689810807f86ce0c693a45
TheDutchPotato <griffin02@hotmail.nl>
2019-12-13 15:49:18 +0100

    Userdashboard

--- Barroc_intense/resources/views/Customer/index.blade.php
+++ Barroc_intense/resources/views/Customer/index.blade.php
@@ -10,11 +10,17 @@
                     <p>Naam: {{$user->name}}</p>
                     <p>Email {{$user->email}}</p>
                     <ul class="list-group" style="max-height:300px; overflow-y: scroll;">
-                    @foreach()
-                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="{{route('maintenance.edit', )}}" class="text-white">Storing</a></li>
-                    @endforeach
+
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+                        <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
+
                     </ul>
-                    <a href="{{route('maintenance.create', )}}" class="text-white">Storing</a>
+                    <a href="{{route('maintenance.create')}}" class="text-white">Storing</a>
                 </div>
             </div>
         </div>
