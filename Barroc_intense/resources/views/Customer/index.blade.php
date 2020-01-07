39ed500ae8cc046645dad3c554667049609fce47
TheDutchPotato <griffin02@hotmail.nl>
2020-01-07 13:10:45 +0100

    small edits

--- Barroc_intense/resources/views/Customer/index.blade.php
+++ Barroc_intense/resources/views/Customer/index.blade.php
@@ -7,9 +7,11 @@
                 <div class="card-header" style="background-color:#ffbf00" >Persoonlijke informatie</div>
                 <div class="card-body row" style="">
 
-                    <p>Naam: {{$user->name}}</p>
-                    <p>Email {{$user->email}}</p>
-                    <ul class="list-group" style="max-height:300px; overflow-y: scroll;">
+                    <div class="info">
+                        <p>Naam: {{$user->name}}</p>
+                        <p>Email {{$user->email}}</p>
+                    </div>
+                    <ul class="list-group " style="max-height:300px; overflow-y: scroll; horiz-align: right">
 
                         <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
                         <li class="list-group-item list-group-item-dark" style="margin: 5px; background-color: #343a40"><a href="" class="text-white">Factuur</a></li>
